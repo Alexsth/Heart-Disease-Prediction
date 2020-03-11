e9a8b430b452637eeb5b0e5b0e75bab23ba726cf
Alexsth <62048913+Alexsth@users.noreply.github.com>
2020-03-11 20:40:55 +0545

    Add files via upload

---
+++ miniproj.py
@@ -0,0 +1,123 @@
+import pandas as pd
+
+from warnings import simplefilter
+simplefilter(action='ignore', category = FutureWarning)
+from warnings import simplefilter
+simplefilter(action='ignore', category = FutureWarning)
+
+
+df = pd.read_csv('cleveland.csv', header = None)
+
+df.columns = ['age', 'sex', 'cp', 'trestbps', 'chol',
+              'fbs', 'restecg', 'thalach', 'exang', 
+              'oldpeak', 'slope', 'ca', 'thal', 'target']
+
+#1 = male, 0 = female
+df.isnull().sum()
+
+df['target'] = df.target.map({0: 0, 1: 1, 2: 1, 3: 1, 4: 1})
+df['sex'] = df.sex.map({0: 'female', 1: 'male'})
+df['thal'] = df.thal.fillna(df.thal.mean())
+df['ca'] = df.ca.fillna(df.ca.mean())
+
+df['sex'] = df.sex.map({'female': 0, 'male': 1})
+
+#SVM
+# data preprocessing
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler as ss
+sc = ss()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+   
+from sklearn.svm import SVC
+classifier = SVC(kernel = 'rbf')
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm_test = confusion_matrix(y_pred, y_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+
+print()
+print('Accuracy for training set for svm = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for svm = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
+
+#Naive Bayes 
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm_test = confusion_matrix(y_pred, y_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+
+print()
+print('Accuracy for training set for Naive Bayes = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for Naive Bayes = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
+#Decision Tree 
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm_test = confusion_matrix(y_pred, y_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+
+print()
+print('Accuracy for training set for Decision Tree = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for Decision Tree = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
+#Random Forest
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.ensemble import RandomForestClassifier
+classifier = RandomForestClassifier(n_estimators = 10)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+cm_test = confusion_matrix(y_pred, y_test)
+
+y_pred_train = classifier.predict(X_train)
+cm_train = confusion_matrix(y_pred_train, y_train)
+
+print()
+print('Accuracy for training set for Random Forest = {}'.format((cm_train[0][0] + cm_train[1][1])/len(y_train)))
+print('Accuracy for test set for Random Forest = {}'.format((cm_test[0][0] + cm_test[1][1])/len(y_test)))
+
